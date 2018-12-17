20edea28e80645478a00d312e92f1ab1523fe72e
Kerem Ataman <kacorperation@gmail.com>
2018-12-17 15:30:31 -0500

    f

--- src/game/GameMain.java
+++ src/game/GameMain.java
@@ -12,7 +12,12 @@ public class GameMain {
 	HashMap<AIBase, char[][]> actionKeeper = new HashMap<AIBase, char[][]> ();
 	
 	// TODO: bot keeper. Keep updating as more classes are added
-	List<AIBase> bots = new ArrayList<AIBase>() {{
+	private static List<AIBase> bots = new ArrayList<AIBase>() {/**
+		 * 
+		 */
+		private static final long serialVersionUID = 940601280556243903L;
+
+	{
 		add(new LeeroyJenkins());
 		add(new RNGesus());
 	}
@@ -22,7 +27,12 @@ public class GameMain {
 	public static void main(String[] args) {
 		// TODO: go through every combination of bots X times
 		for(int matchCounter = 0; matchCounter < 1000; matchCounter++) {
-			
+			for (int i = 0; i < bots.size(); i++) {
+				for(int j = i + 1; j < bots.size(); j++) {
+					// TODO: battler
+					// TODO: if battle is won or lost, or numRounds == 100, or turn taken too long, punish. Tell why
+				}
+			}
 		}
 		// update scores each match
 	}
--- src/game/ScoreKeeper.java
+++ src/game/ScoreKeeper.java
@@ -10,6 +10,35 @@ public class ScoreKeeper {
 		WIN,
 		LOSS
 	}
-	// TODO: record keeper update. Maybe have a class for that guy?
-	HashMap<AIBase, HashMap<AIBase, ArrayList<Outcome>>> scores = new HashMap<AIBase, HashMap<AIBase, ArrayList<Outcome>>>();
+	// TODO: record keeper update.
+	/**
+	 * Has the form get(A)get(B) get A's score vs. B's score. 
+	 * e.g. if get(A)get(B) has win, loss, loss in it, then A won it's first 
+	 * match against B but lost it's other two matches against B
+	 */
+	private HashMap<AIBase, HashMap<AIBase, ArrayList<Outcome>>> scores = new HashMap<AIBase, HashMap<AIBase, ArrayList<Outcome>>>();
+	// TODO: keep a record of the highest scorers, or write a function that gets them when the time comes
+	
+	public void update(AIBase ai1, AIBase ai2, Outcome ai1Outcome, Outcome ai2Outcome) {
+		updateHelper(ai1, ai2, ai1Outcome, ai2Outcome);
+		updateHelper(ai2, ai1, ai2Outcome, ai1Outcome);
+	}
+	
+	private void updateHelper(AIBase ai1, AIBase ai2, Outcome ai1Outcome, Outcome ai2Outcome) {
+		if (scores.get(ai1) == null) {
+			scores.put(ai1, new HashMap<AIBase, ArrayList<Outcome>>());
+		}
+		if(scores.get(ai1).get(ai2) == null) {
+			scores.get(ai1).put(ai2, new ArrayList<Outcome>());
+		}
+		scores.get(ai1).get(ai2).add(ai1Outcome);
+	}
+
+	public HashMap<AIBase, HashMap<AIBase, ArrayList<Outcome>>> getScores() {
+		return scores;
+	}
+
+	public void setScores(HashMap<AIBase, HashMap<AIBase, ArrayList<Outcome>>> scores) {
+		this.scores = scores;
+	}
 }
